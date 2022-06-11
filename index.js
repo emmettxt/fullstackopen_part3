ffc135f3305c5c4082825dfcd96048db72207144
emmettxt <emmet.txt@gmail.com>
2022-06-11 15:39:36 +0100

    Added build

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('build'))
 const morgan = require('morgan')
 app.use(
     morgan((tokens, req, res) =>{
@@ -91,6 +92,6 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = 3005
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
